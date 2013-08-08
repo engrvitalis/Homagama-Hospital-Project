e5dcb5afaf92a7d02cef343103bec2f95cc1e127
hashinisenaratne <hashz1990@gmail.com>
2013-08-08 16:46:21 +0530

    *printing dates inquiry added *test name label changes

--- BaseHospitalHomagama/obj/x86/Debug/BaseHospitalHomagama_Content.g.i.cs
+++ BaseHospitalHomagama/obj/x86/Debug/BaseHospitalHomagama_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/login.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/next.png")]
 
 
